cc9bca28075ab46e1376aebb6a8f7aab8650b155
Jose Giraudo <josegiraudo7@gmail.com>
2022-11-15 17:28:47 -0300

    first commit, create a few components

---
+++ src/components/CharacterList.tsx
@@ -0,0 +1,14 @@
+import { Character } from "./Character"
+
+
+export const CharacterList = () => {
+
+    return(
+        <ul>
+            <Character />
+            <Character />
+            <Character />
+        </ul>
+    )
+
+}
