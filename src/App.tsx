30794ae114c887abba49ee718075b245a6d50277
Jose Giraudo <josegiraudo7@gmail.com>
2022-11-23 09:17:00 -0300

    First draw of the page, will continue after learning React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { CharacterList } from './components/CharacterList';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Characters from Rick and Morty</h1>
       <CharacterList />
+      <Footer />
     </div>
   );
 }
--- src/services/getAllChar.ts
+++ src/services/getAllChar.ts
@@ -1,4 +1,4 @@
 
 export const getAllChar = () => {
-    
+    // https://rickandmortyapi.com/api/character/
 }
